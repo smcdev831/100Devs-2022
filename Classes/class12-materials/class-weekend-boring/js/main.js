240ce67d975446de35220299f4cbb44671f42cba
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-02-17 20:49:52 -0600

    Update main.js

--- Classes/class12-materials/class-weekend-boring/js/main.js
+++ Classes/class12-materials/class-weekend-boring/js/main.js
@@ -4,7 +4,11 @@ function check() {
 
   const day = document.querySelector('#day').value
 
-  //Conditionals go here
-
-
+  if(day === "Saturday" || day === "Sunday"){
+    alert("It's the weekend")
+  }else if (day === "Tuesday" || day === "Thursday"){
+    alert("Class today!")
+  }else{
+    alert("It's just a boring day")
+  }
 }
