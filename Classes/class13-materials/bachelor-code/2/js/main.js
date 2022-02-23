be6e7083fc6d53a1bd6d2c90bba5aa68fea6bc8a
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-02-22 20:19:37 -0600

    Update main.js

--- Classes/class13-materials/bachelor-code/2/js/main.js
+++ Classes/class13-materials/bachelor-code/2/js/main.js
@@ -2,9 +2,9 @@ const andi = document.querySelector('#andi')
 const claire = document.querySelector('#claire')
 const sharleen = document.querySelector('#sharleen')
 
-document.querySelector('#andiNext').addEventListener('click', /*INSERTCODE*/)
-document.querySelector('#claireNext').addEventListener('click', /*INSERTCODE*/)
-document.querySelector('#sharleenNext').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#andiNext').addEventListener('click', andiNext)
+document.querySelector('#claireNext').addEventListener('click', claireNext)
+document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
 
 function andiNext(){
 	claire.classList.add(/*INSERTCODE*/)
@@ -13,13 +13,13 @@ function andiNext(){
 }
 
 function claireNext(){
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
+	claire.classList.add(/*INSERTCODE*/)
+	sharleen.classList.add(/*INSERTCODE*/)
+	andi.classList.toggle(/*INSERTCODE*/)
 }
 
 function sharleenNext(){
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.add(/*INSERTCODE*/)
-	/*INSERTCODE*/.classList.toggle(/*INSERTCODE*/)
+	claire.classList.add(/*INSERTCODE*/)
+	sharleen.classList.add(/*INSERTCODE*/)
+	andi.classList.toggle(/*INSERTCODE*/)
 }
