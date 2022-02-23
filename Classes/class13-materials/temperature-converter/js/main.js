f0da456f26aa529f34a6e570ca116f3022f99c00
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-02-22 20:05:08 -0600

    Update main.js

--- Classes/class13-materials/temperature-converter/js/main.js
+++ Classes/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,14 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+// 0-32
+// need the value in C
+//convert c to f
+//show it
+
+document.querySelector('#yell').addEventListener('click', convert)
+
+function convert() {
+  let temp = document.querySelector('#cel').value
+  temp = temp * 9/5 + 32
+
+  document.querySelector('#placeToYell').innerText = temp
+}
