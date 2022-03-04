139f64b86bd7fa400f1a4a63dd8afeaf7f939422
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-03-03 19:21:37 -0600

    Update main.js

--- Classes/class16-materials/age-checker/js/main.js
+++ Classes/class16-materials/age-checker/js/main.js
@@ -5,6 +5,15 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+let age = 17;
+
+if (age < 16) {
+  console.log("they can not drive");
+} else if (age < 18) {
+  console.log("they can't hate from outside the club, because they can't even get in");
+} else if (age < 21) {
+  console.log("they can not drink");
+}
 
 
 //--- Harder
