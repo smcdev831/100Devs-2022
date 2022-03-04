06171e19c1da0e7eae31647b7c44417124eb6af2
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-03-03 19:31:48 -0600

    Update main.js

--- Classes/class16-materials/age-checker/js/main.js
+++ Classes/class16-materials/age-checker/js/main.js
@@ -13,8 +13,13 @@ if (age < 16) {
   console.log("they can't hate from outside the club, because they can't even get in");
 } else if (age < 21) {
   console.log("they can not drink");
+} else if (age < 25) {
+  console.log("they can not rent cars affordably");
+} else if (age < 30 ) {
+  console.log("they can not rent fancy cars affordably");
+} else if (age >= 30) {
+  console.log("there is nothing left to look forward too which is a lie --100Devs");
 }
-
 
 //--- Harder
 //On click of the h1
