6134dfa20e0f90ff4e8d3e9f2d29dc04dde91008
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-03-15 20:21:18 -0500

    Update main.js

--- Classes/class19-materials/arrays-fight-to-the-finish/js/main.js
+++ Classes/class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,4 +1,6 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
+//Create an array of movie titles. Loop through the array and each element to the h2
+
+let movieTitles["M1", "M2", "M3", "M4"]
 
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
