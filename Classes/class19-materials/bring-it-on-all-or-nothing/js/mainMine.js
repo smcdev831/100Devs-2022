1300896c1bf4a8d189acb7a558e9ac6d857e153d
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-03-15 19:44:33 -0500

    Update mainMine.js

--- Classes/class19-materials/bring-it-on-all-or-nothing/js/mainMine.js
+++ Classes/class19-materials/bring-it-on-all-or-nothing/js/mainMine.js
@@ -36,3 +36,14 @@ numToPower(3, 5)
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
+
+
+function fizzBuzz(n1) {
+  if (n1 % 15 == 0) {
+    console.log(FizzBuzz);
+  } else if (n1 % 5 == 0) {
+    console.log(Buzz);
+  } else if (n1 % 3 == 0) {
+    console.log(Fizz);
+  }
+}
